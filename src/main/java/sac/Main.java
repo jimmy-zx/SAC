b0f66c0a648edc943a6eebcb817c1a2f5b0fc6f2
Jimmy Z <jianjun.zhao@mail.utoronto.ca>
2022-11-15 05:50:20 +0000

    [build] Setting up Maven

---
+++ src/main/java/sac/Main.java
@@ -0,0 +1,7 @@
+package sac;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, SAC!");
+    }
+}
---
+++ src/test/java/sac/SimpleTest.java
@@ -0,0 +1,11 @@
+package sac;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+    @Test
+    void testTrue() {
+        assertTrue(true);
+    }
+}
