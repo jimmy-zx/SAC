652000c266ef6522e185dcf8d341ce74f74af3aa
Jimmy Z <jianjun.zhao@mail.utoronto.ca>
2022-11-19 04:42:50 +0000

    [framework] Updated Model and concrete classes
    - Updated Model class structure
    - Updated GameMode interface
    - Added sample observers

--- src/main/java/sac/model/Model.java
+++ src/main/java/sac/model/Model.java
@@ -1,4 +1,48 @@
 package sac.model;
 
+import sac.model.gamemodes.GameMode;
+import sac.model.generators.Generator;
+import sac.model.observers.RowClearObserver;
+import sac.model.rotations.RotationState;
+
+import java.util.List;
+
 public class Model {
+    private Board board;
+    private Piece currentPiece;
+    private RotationState currentState;
+    private List<RowClearObserver> rowClearObservers;
+    private boolean gameOn;
+    private Generator pieceGenerator;
+    private GameMode gameMode;
+
+    public enum Movement {
+        ROTATE,
+        LEFT,
+        RIGHT,
+        DROP,
+        DOWN,
+        KEEP,
+    }
+
+    public Model() {
+        this.board = new Board();
+        this.gameOn = false;
+    }
+
+    public void setGameMode(GameMode gameMode) {
+        this.gameMode = gameMode;
+    }
+
+    public void newGame() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void modelTick(Movement movement) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean getGameOn() {
+        return this.gameOn;
+    }
 }
--- src/main/java/sac/model/observers/ScoreObserver.java
+++ src/main/java/sac/model/observers/ScoreObserver.java
@@ -1,4 +1,5 @@
 package sac.model.observers;
 
-public abstract class ScoreObserver implements RowClearObserver {
+public abstract class ScoreObserver extends RowClearObserver {
+    public abstract int getScore();
 }
