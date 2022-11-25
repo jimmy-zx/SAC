5f5ff6359815382930973642f8f5e0ab0396133a
Jimmy Z <jianjun.zhao@mail.utoronto.ca>
2022-11-25 14:55:48 -0500

    [framework] Updated Board and Point
    - Implemented Board
    - Updated Point to be a record

--- src/main/java/sac/model/Board.java
+++ src/main/java/sac/model/Board.java
@@ -35,20 +35,39 @@ public class Board {
      * The return status for PlacePiece
      */
     public enum PlacePieceStatus {
-        ADD_OK, ADD_ROW_FILLED, ADD_OUT_BOUNDS, ADD_BAD
+        /**
+         * The placement is successful and no row is filled.
+         */
+        ADD_OK,
+        /**
+         * The placement is successful and some row is filled.
+         */
+        ADD_ROW_FILLED,
+        /**
+         * The placement failed due to out of bound.
+         */
+        ADD_OUT_BOUNDS,
+        /**
+         * The placement failed due to conflict between placed pieces.
+         */
+        ADD_BAD
     }
 
     /**
      * Initializes a new Board object.
      *
-     * @param height The height of the board.
      * @param width The width of the board.
+     * @param height The height of the board.
      */
-    public Board(int height, int width) {
-        this.height = height;
+    public Board(int width, int height) {
         this.width = width;
+        this.height = height;
         this.grid = new Piece.PieceType[width][height];
         this.rowCounts = new int[height];
+    }
+
+    public boolean isValidPoint(Point point) {
+        return 0 <= point.x() && point.x() < width && 0 <= point.y() && point.y() < height;
     }
 
     /**
@@ -63,7 +82,28 @@ public class Board {
      *  ADD_OK in all the other cases.
      */
     public PlacePieceStatus placePiece(Piece piece, Point position) {
-        throw new UnsupportedOperationException();
+        boolean filled = false;
+        for (Point point : piece.body) {
+            Point newPoint = position.add(point);
+            if (!isValidPoint(newPoint)) {
+                return PlacePieceStatus.ADD_OUT_BOUNDS;
+            }
+            if (getGrid(newPoint) != null) {
+                return PlacePieceStatus.ADD_BAD;
+            }
+        }
+        for (Point point : piece.body) {
+            Point newPoint = position.add(point);
+            grid[newPoint.x()][newPoint.y()] = piece.type;
+            rowCounts[newPoint.y()]++;
+            if (rowCounts[newPoint.y()] == width) {
+                filled = true;
+            }
+        }
+        if (filled) {
+            return PlacePieceStatus.ADD_ROW_FILLED;
+        }
+        return PlacePieceStatus.ADD_OK;
     }
 
     /**
@@ -72,23 +112,65 @@ public class Board {
      * @return The number of rows cleared.
      */
     public int clearRows() {
-        throw new UnsupportedOperationException();
+        int skip = 0;  // current number of full rows encountered
+        for (int y = 0; y < height; y++) {
+            if (rowCounts[y] == width) {  // a full row
+                for (int x = 0; x < width; x++) {
+                    grid[x][y] = null;
+                }
+                skip++;
+            } else if (skip > 0) {  // pull this row down
+                for (int x = 0; x < width; x++) {
+                    grid[x][y - skip] = grid[x][y];
+                }
+            }
+        }
+        // renew the rowCount
+        for (int y = 0; y < height; y++) {
+            int count = 0;
+            for (int x = 0; x < width; x++) {
+                if (grid[x][y] != null) {
+                    count++;
+                }
+            }
+            rowCounts[y] = count;
+        }
+        return skip;
     }
 
     /**
      * Backup the grid.
+     * <p>
      * This function should be only called by the client.
      */
     public void backup() {
-        throw new UnsupportedOperationException();
+        if (backupGrid != null) {
+            throw new RuntimeException("Cannot backup the grid. Commit or undo first.");
+        }
+        backupGrid = grid.clone();
     }
 
     /**
      * Restore the grid before the backup.
+     * <p>
      * This function should be only called by the client.
      */
     public void undo() {
-        throw new UnsupportedOperationException();
+        if (this.backupGrid == null) {
+            throw new RuntimeException("Nothing to undo.");
+        }
+        grid = backupGrid;
+        commit();
+    }
+
+    /**
+     * Commit the change.
+     */
+    public void commit() {
+        if (backupGrid == null) {
+            throw new RuntimeException("Nothing to commit");
+        }
+        backupGrid = null;
     }
 
     /**
@@ -98,7 +180,7 @@ public class Board {
      * @return The type of piece that occupies the position. null if no piece occupies such position.
      */
     public Piece.PieceType getGrid(Point pos) {
-        return this.grid[pos.x][pos.y];
+        return grid[pos.x()][pos.y()];
     }
 
     /**
@@ -107,6 +189,18 @@ public class Board {
      * @return A string representation of the board.
      */
     public String toString() {
-        throw new UnsupportedOperationException();
+        StringBuffer builder = new StringBuffer();
+        builder.append(String.format("Board with (%d, %d)\n", height, width));
+        for (int y = height - 1; y >= 0; y--) {
+            for (int x = 0; x < width; x++) {
+                if (grid[x][y] == null) {
+                    builder.append(" ");
+                } else {
+                    builder.append(grid[x][y].ordinal());
+                }
+            }
+            builder.append('\n');
+        }
+        return builder.toString();
     }
 }
