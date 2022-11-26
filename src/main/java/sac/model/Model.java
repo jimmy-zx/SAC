23810affa788112a5e7ba3b9ffce59fd73798527
Jimmy Z <jianjun.zhao@mail.utoronto.ca>
2022-11-26 22:16:50 +0000

    [framework] Moved some feature from Model to GameMode
    - Updated Model
    - Updated GameMode
    - Added documentation for GameMode

--- src/main/java/sac/model/Model.java
+++ src/main/java/sac/model/Model.java
@@ -1,22 +1,19 @@
 package sac.model;
 
 import sac.model.gamemodes.GameMode;
-import sac.model.generators.Generator;
-import sac.model.observers.RowClearObserver;
-import sac.model.observers.ScoreObserver;
 import sac.model.rotations.RotationState;
 
-import java.util.List;
 
+/**
+ * The Tetris Game model.
+ * <p>
+ * Serves a "driver" for the game. The game details are controlled by GameMode
+ */
 public class Model {
     private Board board;
     private Piece currentPiece;
     private RotationState currentState;
-    private List<Piece> preview;
-    private List<RowClearObserver> rowClearObservers;
-    private int score;
     private boolean gameOn;
-    private Generator pieceGenerator;
     private GameMode gameMode;
 
     public enum Movement {
@@ -43,30 +40,27 @@ public class Model {
     }
 
     public void modelTick(Movement movement) {
+        /*
+         * 1. Check gameOn and gameMode.isGameOn(). If false, return.
+         * 2. Perform movement on currentPiece.
+         * 3. If the movement is valid, return.
+         * 4. If the movement is invalid, and the movement is not down, return.
+         * 5. If the movement is invalid, and the movement is down, generate a new Piece.
+         */
         throw new UnsupportedOperationException();
     }
 
-    public boolean getGameOn() {
+    public boolean isGameOn() {
         return this.gameOn;
+    }
+
+    public void setGameOn(boolean gameOn) {
+        this.gameOn = gameOn;
     }
 
     public void putGhostPiece() {throw new UnsupportedOperationException();}
 
-    public void notifyScoreObservers() {
-        for (RowClearObserver observer : rowClearObservers) {
-            observer.update(this);
-        }
-    }
-
-    private void calculateScore() {
-        for (RowClearObserver observer : rowClearObservers) {
-            if (observer instanceof ScoreObserver) {
-                this.score += ((ScoreObserver) observer).getScore();
-            }
-        }
-    }
-
     public int getScore() {
-        return this.score;
+        return gameMode.getScore();
     }
 }
