2daef90d45f8aeab40e03d6e4b16102a31fa0601
Stella <sqstella.chen@mail.utoronto.ca>
2022-12-05 16:56:19 -0500

    [DEV-1.1] Updated ScoreCalculator

--- src/main/java/sac/model/observers/ScoreCalculator.java
+++ src/main/java/sac/model/observers/ScoreCalculator.java
@@ -1,5 +1,5 @@
 package sac.model.observers;
 
-public abstract class ScoreObserver implements Observer<DataPackage> {
+public abstract class ScoreCalculator implements Observer<DataPackage> {
     public abstract int getScore();
 }
