66bf3443f34d676caa6bb092b147d4c69260df0e
liujunx3 <junxi.liu@mail.utoronto.ca>
2022-12-06 03:00:36 -0500

    [DEV-1.4] Add doc to Colorscheme

--- src/main/java/sac/model/colors/Colorscheme.java
+++ src/main/java/sac/model/colors/Colorscheme.java
@@ -8,7 +8,15 @@ import sac.model.Piece;
  */
 public interface Colorscheme {
 
+    /**
+     * Render different pieces with different color.
+     * @param type - the type of piece to be rendered
+     * @return the color of current piece
+     */
     Color render(Piece.PieceType type);
 
+    /**
+     * @return the color of the background (Tetris board)
+     */
     Color backgroundColor();
 }
