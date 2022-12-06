b73ee50e2051be2d3892a8cef01fd35c31314717
Chocaholics <stellachen2025@gmail.com>
2022-12-06 16:52:01 -0500

    [DEV-1.1] Added BackToBackObserverTest

---
+++ src/test/java/sac/model/observers/BackToBackObserverTest.java
@@ -0,0 +1,34 @@
+package sac.model.observers;
+
+import org.junit.jupiter.api.Test;
+import sac.model.Model;
+import sac.model.Piece;
+import sac.model.rotations.SuperRotationSystem;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BackToBackObserverTest {
+    @Test
+    void testBackToBackObserver() {
+        ScoreObserver observer = new BackToBackObserver();
+        DataPackage dataPackage = new DataPackage();
+        Piece piece = Piece.generate(Piece.PieceType.T);
+        SuperRotationSystem.SuperRotationState rotationState = SuperRotationSystem.SuperRotationState.OState;
+        dataPackage.activePiece = piece;
+        dataPackage.rotationState = rotationState;
+        dataPackage.validMove = true;
+        rotationState.getRotationOffset(piece, Model.MoveType.ROTATE_LEFT);
+        rotationState.getRotationOffset(piece, Model.MoveType.ROTATE_LEFT);
+        assertEquals(2, rotationState.getTestCnt());
+        observer.update(dataPackage);
+        assertEquals(0, observer.getScore());
+        dataPackage.rowCleared = 4;
+        observer.update(dataPackage);
+        assertEquals(1000, observer.getScore());
+        dataPackage.rowCleared = 0;
+        observer.update(dataPackage);
+        assertEquals(1000, observer.getScore());
+        observer.update(dataPackage);
+        assertEquals(0, observer.getScore());
+    }
+}
