ca50a305f8dc3c24dde3af5c7f1199bfb16d2891
liujunx3 <junxi.liu@mail.utoronto.ca>
2022-12-06 03:01:22 -0500

    [DEV-1.4] Fix bug in ClassicColor

--- src/main/java/sac/model/colors/ClassicColor.java
+++ src/main/java/sac/model/colors/ClassicColor.java
@@ -3,19 +3,26 @@ package sac.model.colors;
 import javafx.scene.paint.Color;
 import sac.model.Piece;
 
+/**
+ * The color scheme that a classic Tetris game would use.
+ */
 public class ClassicColor implements Colorscheme {
+    @Override
+    public Color render(Piece.PieceType type) {
+        switch (type) {
+            case I: return Color.LIGHTBLUE;
+            case J: return Color.DARKBLUE;
+            case L: return Color.ORANGE;
+            case O: return Color.YELLOW;
+            case S: return Color.GREEN;
+            case Z: return Color.RED;
+            case T: return Color.MAGENTA;
+        }
+        return Color.WHITE;
+    }
 
     @Override
-    public Color render(Piece piece) {
-        return switch (piece.type) {
-            case S -> Color.GREEN;
-            case I -> Color.LIGHTBLUE;
-            case J -> Color.BLUE;
-            case L -> Color.ORANGE;
-            case O -> Color.YELLOW;
-            case T -> Color.PURPLE;
-            case Z -> Color.RED;
-            default -> Color.BLACK;
-        };
+    public Color backgroundColor() {
+        return Color.GRAY;
     }
 }
