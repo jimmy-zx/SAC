6fc51a862066444a26776b45b352c728735821d6
liujunx3 <junxi.liu@mail.utoronto.ca>
2022-12-06 02:59:54 -0500

    [DEV-1.4] Update Colorscheme interface

--- src/main/java/sac/model/colors/Colorscheme.java
+++ src/main/java/sac/model/colors/Colorscheme.java
@@ -3,6 +3,12 @@ package sac.model.colors;
 import javafx.scene.paint.Color;
 import sac.model.Piece;
 
+/**
+ * Define a color scheme.
+ */
 public interface Colorscheme {
-    Color render(Piece piece);
+
+    Color render(Piece.PieceType type);
+
+    Color backgroundColor();
 }
